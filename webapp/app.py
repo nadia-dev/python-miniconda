3e233381b60d753f95b12bf620239eb758208e45
Nadia Yudina <joystate@me.com>
2019-04-13 21:20:02 -0400

    add my code to app

--- webapp/app.py
+++ webapp/app.py
@@ -1,18 +1,42 @@
-from flask import Flask, jsonify
-from sklearn import datasets, svm
+from flask import Flask
+from flask import request
+from pathlib import Path
+from string import Template
 
 app = Flask(__name__)
+logs = 'logs.txt'
 
-# Load Dataset from scikit-learn.
-digits = datasets.load_digits()
+'''
+Calls:
+1. source -> read, activity, average (read), value (read)
+2. source -> calculated, activity, average (updated), value (read), most_recent (value read before), fuel (calculated)
+3. source -> granted, activity, fuel (granted)
+'''
 
-@app.route('/')
-def hello():
-    clf = svm.SVC(gamma=0.001, C=100.)
-    clf.fit(digits.data[:-1], digits.target[:-1])
-    prediction = clf.predict(digits.data[-1:])
+@app.route('/log')
+def log():
 
-    return jsonify({'prediction': repr(prediction)})
+    filename = Path(logs)
+    filename.touch(exist_ok=True)  # will create file, if it exists will do nothing
+    file = open(logs, 'a')
+
+    source = request.args.get('source') # source: read, calculated, granted
+    activity = request.args.get('activity')
+    average = request.args.get('average')
+    value = request.args.get('value')
+    fuel = request.args.get('fuel')
+    most_recent = request.args.get('mostRecentReading')
+    date = request.args.get('date')
+
+    template = Template("Source: $a, Activity: $b, Average: $c, Value: %d, Most Recent: %e, Fuel: %f, datetime: %g")
+    info = template.safe_substitute(a=source, b=activity, c=average, d=value, e=most_recent, f=fuel, g=date)
+    info += "\n"
+    info += '-------------------\n'
+
+    file.write(info)
+    file.close()
+
+    return 'OK'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
